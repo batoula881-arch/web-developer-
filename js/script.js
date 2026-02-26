12f341a271312c7182022b060e0adb07eb83727f
batoula881-arch <batoula881@gmail.com>
2026-02-27 01:15:37 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let icon = document.getElementById('icon-mobile');
+let ul = document.getElementById('ul-mobile');
+
+
+icon.onclick = function () {
+    ul.classList.toggle("mobile-nav1");
+}
+
+
+
+
+window.$crisp=[];window.CRISP_WEBSITE_ID="2730fd14-38af-40c2-b3bf-003b3447f73f";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
